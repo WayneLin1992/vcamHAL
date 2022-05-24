12ae7842486fd54309dfe2860ab37c386762fae8
Wei-Han Lin <62940642+WayneLin1992@users.noreply.github.com>
2022-05-24 10:22:37 +0800

    implement ioctl

--- v4l2_wrapper.cpp
+++ v4l2_wrapper.cpp
@@ -14,3 +14,28 @@ int V4L2Wrapper::IoctlLock(unsigned long request, T data){
     return ioctl(device_fd_.get(), request, data);
 }
 
+int V4L2Wrapper::StreamOn() {
+  
+  int32_t type = format_->type();
+  if (IoctlLocked(VIDIOC_STREAMON, &type) < 0) {
+    HAL_LOGE("STREAMON fails (%d): %s", errno, strerror(errno));
+    return -ENODEV;
+  }
+
+  HAL_LOGV("Stream turned on.");
+  return 0;
+}
+
+int V4L2Wrapper::GetFormats(std::set<uint32_t>* v4l2_formats) {
+    
+  v4l2_fmtdesc format_query;
+  memset(&format_query, 0, sizeof(format_query));
+    
+  format_query.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  while (IoctlLocked(VIDIOC_ENUM_FMT, &format_query) >= 0) {
+    v4l2_formats->insert(format_query.pixelformat);
+    ++format_query.index;
+  }
+    
+  return 0;
+}
